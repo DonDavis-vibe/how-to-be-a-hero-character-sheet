693fe1cfc2a763e617647385be820d9c00cd83ba
DonDavis-vibe <260029956+DonDavis-vibe@users.noreply.github.com>
2026-08-07 15:07:47 +0200

    chore: clean up temporary compression scripts

--- check.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$sourceDir = 'E:\vibe_coding\rocketbeans\logo\comic'
-Get-ChildItem -Path $sourceDir -Filter '*.png' | ForEach-Object {
-    $img = [System.Drawing.Image]::FromFile($_.FullName)
-    Write-Host "$($_.Name): $($img.Width)x$($img.Height)"
-    $img.Dispose()
-}
